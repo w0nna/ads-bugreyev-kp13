2f93e37bc834fca76e698bbd695d9a036cdb5229
DESKTOP-FGOS0RV\1wni Work <bugreyev.glebkp13@gmail.com>
2022-08-01 21:44:46 +0300

    Лабороторна робота №6 завершена.

--- Lab06/lab06f/lab06f/MainWindow.xaml.cs
+++ Lab06/lab06f/lab06f/MainWindow.xaml.cs
@@ -16,11 +16,9 @@ using System.Windows.Shapes;
 
 namespace lab06f
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        int o = 0;
         public MainWindow()
         {
             InitializeComponent();
@@ -47,223 +45,132 @@ namespace lab06f
             PrimerBox.Text = "";
         }
 
-        private void Calc_Btn_Click(object sender, RoutedEventArgs e)
+        public static string ToRPN(string initialString)
         {
-            BorderLeft.Visibility = Visibility.Visible;
-            BorderRight.Visibility = Visibility.Visible;
-            String func = PrimerBox.Text;
+            Stack<char> operationsStack = new Stack<char>();
 
-            List<string> arguments = new List<string>();
-            string operand1 = "";
-            string operand2 = "";
-            string lastOperation = "";
-            List<string> operations = new List<string> { "(", "+", "-", "*", "/", "^", ")", "(", "()" };
-            List<string> functions = new List<string> { "sin", "cos", "exp", "tan", "ln", "lg", "sh", "ch" };
+            char lastOperation;
 
-            int symbolNumber = 0;
-            String sourceExpression = PrimerBox.Text.Replace(" ", "");
+            string result = string.Empty;
+            
+            initialString = initialString.Replace(" ", "");
 
-            while (symbolNumber < sourceExpression.Length)
+            for (int i = 0; i < initialString.Length; i++)
             {
-                int currentIndex = arguments.Count - 1;
-
-                //Open brackets
-                if (sourceExpression[symbolNumber] == '(')
+                if (Char.IsDigit(initialString[i]))
                 {
-                    arguments.Add(sourceExpression[symbolNumber].ToString());
-                    while (operations.Contains(arguments[currentIndex]) &&
-                               operations.LastIndexOf(sourceExpression[symbolNumber].ToString()) >
-                               operations.IndexOf(arguments[currentIndex]))
-                    {
-                        arguments[currentIndex + 1] = arguments[currentIndex];
-                        currentIndex--;
-                    }
-
-                    arguments[currentIndex + 1] = sourceExpression[symbolNumber].ToString();
-                    lastOperation = sourceExpression[symbolNumber].ToString();
-                    symbolNumber++;
+                    result += initialString[i];
                     continue;
                 }
-                //Close brackets
-                else if (sourceExpression[symbolNumber] == ')')
-                {
-                    arguments[arguments.LastIndexOf("(")] = "()";
-                    lastOperation = "()";
-                    symbolNumber++;
-                    continue;
-                }
-                //Operation
-                else if (operations.Contains(sourceExpression[symbolNumber].ToString()))
-                {
-                    //Always after (
-                    if (lastOperation == "(")
-                    {
-                        int index = arguments.LastIndexOf(lastOperation);
-                        arguments.Add(lastOperation);
-                        while (currentIndex >= index)
-                        {
-                            arguments[currentIndex + 1] = arguments[currentIndex];
-                            currentIndex--;
-                        }
 
-                        arguments[currentIndex + 1] = sourceExpression[symbolNumber].ToString();
-                    }
-                    //priority
+                if (IsOperation(initialString[i]))
+                {
+                    if (!(operationsStack.Count == 0))
+                        lastOperation = operationsStack.Peek();
                     else
                     {
-                        arguments.Add(lastOperation.ToString());
-                        while (
-                            operations.Contains(arguments[currentIndex]) &&
-                            operations.IndexOf(sourceExpression[symbolNumber].ToString()) >
-                            operations.IndexOf(arguments[currentIndex]))
-                        {
-                            arguments[currentIndex + 1] = arguments[currentIndex];
-                            currentIndex--;
-                        }
-
-                        arguments[currentIndex + 1] = sourceExpression[symbolNumber].ToString();
+                        operationsStack.Push(initialString[i]);
+                        continue;
                     }
-
-                    lastOperation = sourceExpression[symbolNumber].ToString();
-                    symbolNumber++;
-                }
-                //Operand1
-                else if (operand1 == "")
-                {
-                    while (symbolNumber < sourceExpression.Length &&
-                        !operations.Contains(sourceExpression[symbolNumber].ToString()))
+                    if (GetOperationPriority(lastOperation) < GetOperationPriority(initialString[i]))
                     {
-                        operand1 += sourceExpression[symbolNumber];
-                        symbolNumber++;
-                    }
-
-                    arguments.Add(operand1);
-                    if (functions.Contains(operand1))
-                    {
-                        arguments.Add("Compose");
-                    }
-                    operand2 = "";
-                }
-                //Operand2
-                else if (operand2 == "")
-                {
-                    while (symbolNumber < sourceExpression.Length &&
-                        !operations.Contains(sourceExpression[symbolNumber].ToString()))
-                    {
-                        operand2 += sourceExpression[symbolNumber];
-                        symbolNumber++;
-                    }
-
-                    int index = arguments.LastIndexOf(lastOperation.ToString());
-                    int bracketIndex = arguments.IndexOf("(");
-                    if (bracketIndex > 0 &&
-                        arguments.LastIndexOf(lastOperation.ToString(), bracketIndex) > 0 &&
-                        arguments.LastIndexOf(lastOperation.ToString(), bracketIndex) < index)
-                    {
-                        index = arguments.LastIndexOf(lastOperation.ToString(), arguments.IndexOf("("));
-                    }
-                    arguments.Add(operand2);
-                    if (functions.Contains(operand2))
-                    {
-                        arguments.Add("Compose");
-                    }
-
-                    #region Put arg2 at right place
-                    currentIndex = arguments.Count - 1;
-                    while (currentIndex > index)
-                    {
-                        if (functions.Contains(operand2))
-                        {
-                            arguments[currentIndex] = arguments[currentIndex - 2];
-                        }
-                        else
-                        {
-                            arguments[currentIndex] = arguments[currentIndex - 1];
-                        }
-                        currentIndex--;
-                    }
-
-                    arguments[index] = operand2;
-                    if (functions.Contains(operand2))
-                    {
-                        arguments[index + 1] = "Compose";
-                    }
-                    #endregion
-
-                    operand2 = "";
-                }
-                TextLabelLeft.Text += String.Join(",", arguments) + Environment.NewLine;
-            }
-
-            //ResultTextBox.Text = "";
-            TextLabelRight.Text = String.Join(",", arguments);
-
-            Dictionary<string, string> DiffsList = new Dictionary<string, string>();
-
-            //Calculating f'
-            while (arguments.Count > 1)
-            {
-                String firstOper = "";
-                int firstOperIndex = arguments.Count;
-                foreach (String str in operations)
-                {
-                    if (arguments.IndexOf(str) > 0 && arguments.IndexOf(str) < firstOperIndex)
-                    {
-                        firstOperIndex = arguments.IndexOf(str);
-                        firstOper = str;
-                    }
-                }
-
-                String f1 = "", f2 = "", df1 = "", df2 = "", oper = "", expression = "", diff = "";
-
-                if (firstOper == "()")
-                {
-                    arguments.RemoveAt(firstOperIndex);
-                    int ComposeIndex = arguments.LastIndexOf("Compose", firstOperIndex);
-                    if (ComposeIndex > 0)
-                    {
-                        f1 = arguments[ComposeIndex - 1];
-                        df1 = Proizv.Diff(f1, DiffsList);
-                        f2 = arguments[ComposeIndex + 1];
-                        df2 = Proizv.Diff(f2, DiffsList);
-                        oper = "Compose";
-                        expression = f1 + "(" + f2 + ")";
-                        diff = Proizv.Diff(f1, df1, f2, df2, oper);
-
-                        arguments.RemoveAt(ComposeIndex + 1);
-                        arguments.RemoveAt(ComposeIndex);
-                        arguments[ComposeIndex - 1] = expression;
+                        operationsStack.Push(initialString[i]);
+                        continue;
                     }
                     else
                     {
+                        result += operationsStack.Pop();
+                        operationsStack.Push(initialString[i]);
                         continue;
                     }
                 }
+
+                if (initialString[i].Equals('('))
+                {
+                    operationsStack.Push(initialString[i]);
+                    continue;
+                }
+                if (initialString[i].Equals(')'))
+                {
+                    while (operationsStack.Peek() != '(')
+                    {
+                        result += operationsStack.Pop();
+                    }
+                    operationsStack.Pop();
+                }
+            }
+            while (!(operationsStack.Count == 0))
+            {
+                result += operationsStack.Pop();
+            }
+
+            return result;
+        }
+
+
+        public static int CalculateRPN(string rpnString)
+        {
+            Stack<int> numbersStack = new Stack<int>();
+
+            int op1, op2;
+
+            for (int i = 0; i < rpnString.Length; i++)
+            {
+
+                if (Char.IsDigit(rpnString[i]))
+                    numbersStack.Push(int.Parse(rpnString[i].ToString()));
                 else
                 {
-                    f1 = arguments[firstOperIndex - 2];
-                    df1 = Proizv.Diff(f1, DiffsList);
-                    f2 = arguments[firstOperIndex - 1];
-                    df2 = Proizv.Diff(f2, DiffsList);
-                    oper = arguments[firstOperIndex];
-                    expression = f1 + oper + f2;
-                    diff = Proizv.Diff(f1, df1, f2, df2, oper);
-
-                    arguments.RemoveAt(firstOperIndex);
-                    arguments.RemoveAt(firstOperIndex - 1);
-                    arguments[firstOperIndex - 2] = expression;
-                }
-
-                TextLabelLeft.Text += Environment.NewLine + expression;
-                TextLabelRight.Text += Environment.NewLine + diff;
-
-                if (!DiffsList.ContainsKey(expression))
-                {
-                    DiffsList.Add(expression, diff);
+                    op2 = numbersStack.Pop();
+                    op1 = numbersStack.Pop();
+                    numbersStack.Push(ApplyOperation(rpnString[i], op1, op2));
                 }
             }
 
+            return numbersStack.Pop();
+        }
 
+        private static bool IsOperation(char c)
+        {
+            if (c == '+' ||
+                c == '-' ||
+                c == '*' ||
+                c == '/')
+                return true;
+            else
+                return false;
+        }
+
+
+        private static int GetOperationPriority(char c)
+        {
+            switch (c)
+            {
+                case '+': return 1;
+                case '-': return 1;
+                case '*': return 2;
+                case '/': return 2;
+                default: return 0;
+            }
+        }
+
+     
+        private static int ApplyOperation(char operation, int op1, int op2)
+        {
+            switch (operation)
+            {
+                case '+': return (op1 + op2);
+                case '-': return (op1 - op2);
+                case '*': return (op1 * op2);
+                case '/': return (op1 / op2);
+                default: return 0;
+            }
+        }
+
+
+        private void Calc_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            TextLabelLeft.Text = ToRPN(PrimerBox.Text);
         }
 
     }
